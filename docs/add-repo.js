819fa49581ca4aa6f2e9f3a2c9ae66f29d82c664
rockythekidlock <rockythekidlock@users.noreply.github.com>
2026-07-13 19:37:39 -0600

    Restore external extension repositories

--- docs/add-repo.js
+++ docs/add-repo.js
@@ -1,12 +1,22 @@
 const repoUrls = {
   manga: "https://manatan-community.github.io/extensions/manga.min.json",
   video: "https://manatan-community.github.io/extensions/video.min.json",
-  novel: "https://manatan-community.github.io/extensions/novel.min.json"
+  novel: "https://manatan-community.github.io/extensions/novel.min.json",
+  mihonManga: "https://raw.githubusercontent.com/keiyoushi/extensions/repo/index.min.json",
+  aidokuManga: "https://aidoku-community.github.io/sources/index.min.json",
+  aniyomiVideo: "https://raw.githubusercontent.com/yuzono/anime-repo/repo/index.min.json"
 };
 
 const reposByMedia = {
-  manga: [{ label: "Manatan manga", url: repoUrls.manga }],
-  video: [{ label: "Manatan video", url: repoUrls.video }],
+  manga: [
+    { label: "Manatan manga", url: repoUrls.manga },
+    { label: "Mihon Manga Extensions", url: repoUrls.mihonManga },
+    { label: "Aidoku Manga Sources", url: repoUrls.aidokuManga }
+  ],
+  video: [
+    { label: "Manatan video", url: repoUrls.video },
+    { label: "Aniyomi Video Extensions", url: repoUrls.aniyomiVideo }
+  ],
   novel: [
     { label: "Manatan novel", url: repoUrls.novel }
   ]
